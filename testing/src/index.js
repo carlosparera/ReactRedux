6b9b9b89d5cf344ddad86d2806aa2d4ba0b1aeca
Parera <carlospareraalvarez@gmail.com>
2018-09-02 11:29:58 +0200

    ´testing´

--- testing/src/index.js
+++ testing/src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import Root from 'Root';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import App from 'components/App';
+
+ReactDOM.render(
+  <Root>
+    <App />
+  </Root>,
+  document.querySelector('#root')
+);
